d392d58a03f7d51cc8e79f15fb94e7d4e6decbaf
H-Asmaa <asmaehadar32@gmail.com>
2024-02-13 19:49:39 +0100

    Committing starwars api, a script that prints all the characters of a Star Wars Movie.

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,38 @@
+#!/usr/bin/node
+/**
+ * A script that prints all the characters of a Star Wars Movie.
+ */
+const request = require('request');
+const movieId = process.argv[2];
+
+if (!movieId) {
+  console.log('Please provide a movie ID');
+  process.exit(1);
+}
+
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request.get(url, async (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    try {
+      const fetchCharacterName = (characterUrl) => {
+        return new Promise((resolve, reject) => {
+          request.get(characterUrl, (error, response, body) => {
+            if (!error && response.statusCode === 200) {
+              resolve(JSON.parse(body).name);
+            } else {
+              reject('Character information request failed!');
+            }
+          });
+        });
+      };
+      for (const characterUrl of JSON.parse(body).characters) {
+        console.log(await fetchCharacterName(characterUrl));
+      }
+    } catch (err) {
+      console.log('Exception:', err);
+    }
+  } else {
+    console.log('Movie information request failed!');
+  }
+});
