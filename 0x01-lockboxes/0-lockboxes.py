ea7c96e397ffdfc0c09db1a309f5c54ad8c3a8de
H-Asmaa <asmaehadar32@gmail.com>
2024-02-23 18:56:39 +0100

    Committing A function that determines if all the boxes can be opened.

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def canUnlockAll(boxes):
+    """A function that determines if all the boxes can be opened."""
     unlocked_boxes = {0}  # Start with box 0 unlocked
     prev_count = 0
 
@@ -10,13 +11,3 @@ def canUnlockAll(boxes):
             unlocked_boxes.update(boxes[box])
 
     return len(unlocked_boxes) == len(boxes)
-
-# Test cases
-boxes1 = [[1], [2], [3], [4], []]
-print(canUnlockAll(boxes1))  # Should print True
-
-boxes2 = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-print(canUnlockAll(boxes2))  # Should print True
-
-boxes3 = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-print(canUnlockAll(boxes3))  # Should print False
