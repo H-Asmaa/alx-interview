b6db9b32192780561a28eb7f7a6127121f5e8a87
H-Asmaa <asmaehadar32@gmail.com>
2024-02-28 06:24:51 +0100

    Committing a function that determine the fewest number of coins needed to meet a given amount total.

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,6 +7,8 @@
 def makeChange(coins, total):
     """A function that determine the fewest number of coins
     needed to meet a given amount total."""
+    if total < 0 or total == 0:
+        return 0
     coins = sorted(coins)[::-1]
     count = i = 0
     length = len(coins)
@@ -21,3 +23,5 @@ def makeChange(coins, total):
     if total:
         return -1
     return count
+
+print(makeChange([65, 67, 8], 0))
