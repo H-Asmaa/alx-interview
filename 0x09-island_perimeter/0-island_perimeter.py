944a3c4f63f627cb907c85fccbb0239cfd5df912
H-Asmaa <asmaehadar32@gmail.com>
2024-03-07 18:42:26 +0100

    Committing a function that calculates the island perimeter.
    Adding a list of lists type check.

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -6,13 +6,15 @@
 
 def island_perimeter(grid):
     """A function that calculates the island perimeter."""
-    perimeter = 0
-    for row in range(len(grid)):
-        for col in range(len(grid[row])):
-            if grid[row][col]:
-                perimeter += 4
-                if grid[row][col - 1]:
-                    perimeter -= 2
-                if grid[row - 1][col]:
-                    perimeter -= 2
-    return perimeter
+    if isinstance(grid, list) and all(isinstance(sublist, list) for sublist in grid):
+        if isinstance(grid, list):
+            perimeter = 0
+            for row in range(len(grid)):
+                for col in range(len(grid[row])):
+                    if grid[row][col]:
+                        perimeter += 4
+                        if col and grid[row][col - 1]:
+                            perimeter -= 2
+                        if row > 0 and grid[row - 1][col]:
+                            perimeter -= 2
+            return perimeter
