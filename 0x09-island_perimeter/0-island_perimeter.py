dcff6dd91c62cf0ed488d9f6db6718bf043f1014
H-Asmaa <asmaehadar32@gmail.com>
2024-03-07 17:14:04 +0100

    Committing a function that calculates the island perimeter.
    Fixing variable name, I changed it to be more relatable to the function but i forgot to change it for the rest of the apearences.

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -10,9 +10,9 @@ def island_perimeter(grid):
     for row in range(len(grid)):
         for col in range(len(grid[row])):
             if grid[row][col]:
-                count += 4
+                perimeter += 4
                 if grid[row][col - 1]:
-                    count -= 2
+                    perimeter -= 2
                 if grid[row - 1][col]:
-                    count -= 2
+                    perimeter -= 2
     return perimeter
