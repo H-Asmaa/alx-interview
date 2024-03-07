77842211d3454adf0c8b4c8d6cd1bbdbda17d619
H-Asmaa <asmaehadar32@gmail.com>
2024-03-07 17:11:17 +0100

    Committing a function that calculates the island perimeter.

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+0x09. Island Perimeter
+"""
+
+
+def island_perimeter(grid):
+    """A function that calculates the islandd perimeter."""
+    count = 0
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if grid[row][col]:
+                count += 4
+                if grid[row][col - 1]:
+                    count -= 2
+                if grid[row - 1][col]:
+                    count -= 2
+    return count
