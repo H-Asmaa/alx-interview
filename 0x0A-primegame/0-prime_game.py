f9dc6d4d87df20ce9dfe4c5a80320c3e3f89bde2
H-Asmaa <asmaehadar32@gmail.com>
2024-03-14 08:34:52 +0000

    Committing code refactor, fixing output fails.

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,33 +4,38 @@
 """
 
 
+def isPrime(n):
+    """A function that determins if a number is prime."""
+    if n == 2:
+        return True
+    if n < 2:
+        return False
+    for i in range(3, int(n**0.5) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
+
+
 def isWinner(x, nums):
     """A function that determines the winner of the prime games."""
-    turn = 0
-    maria_score = ben_score = 0
-    while nums:
-        prime = None
-        for num in nums:
-            if num == 2:
-                prime = num
-                break
-            if num < 2 or num % 2 == 0:
+    tmp_list = []
+    ben_score = maria_score = 0
+    for num in nums[:x]:
+        turn = 0
+        for i in range(1, num + 1):
+            tmp_list.append(i)
+        for pick in tmp_list[:]:
+            if not isPrime(pick):
                 continue
-            for n in range(3, int(num**0.5) + 1, 2):
-                if num % n == 0:
-                    break
-            prime = num
-            break
-        if prime is None:
-            break
-        nums.remove(prime)
+            tmp_list.remove(pick)
+            turn = 1 - turn
         if turn:
             maria_score += 1
         else:
             ben_score += 1
-        for n in nums[:]:
-            if n % prime == 0:
-                nums.remove(n)
-        turn = 1 - turn
-        x -= 1
-    return "Maria" if maria_score > ben_score else "Ben"
+    if maria_score > ben_score:
+        return "Maria"
+    elif maria_score < ben_score:
+        return "Ben"
+    else:
+        return None
